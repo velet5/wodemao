1419d1f523f589c9a3993a9f4dff7a1433b1989a
Oleg Arshinskii <velet592@gmail.com>
2023-01-30 21:03:00 +0200

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <section class="logo">
+      <img src="@/assets/cat.png" />
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+.logo img {
+  width: 300px;
+}
+</style>
